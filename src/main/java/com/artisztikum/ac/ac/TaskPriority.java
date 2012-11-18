6aa698a0ddbba74d81dacbb95bc7da3726b40568
Adam Dajka <dajka.adam@gmail.com>
2012-11-18 21:35:39 +0100

    Removed Checkstyle warnings.

--- src/main/java/com/artisztikum/ac/ac/TaskPriority.java
+++ src/main/java/com/artisztikum/ac/ac/TaskPriority.java
@@ -2,12 +2,35 @@ package com.artisztikum.ac.ac;
 
 /**
  * Ticket priorities.
- * 
+ *
  * @author adam.dajka
- * 
+ *
  */
 @SuppressWarnings("all")
 public enum TaskPriority
 {
-	Highest, High, Normal, Low, Lowest,
+	/**
+	 * Priority level 2.
+	 */
+	Highest,
+
+	/**
+	 * Priority level 1.
+	 */
+	High,
+
+	/**
+	 * Priority level 0.
+	 */
+	Normal,
+
+	/**
+	 * Priority level -1.
+	 */
+	Low,
+
+	/**
+	 * Priority level -2.
+	 */
+	Lowest,
 }
